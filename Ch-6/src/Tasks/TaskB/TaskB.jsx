9fe8dbedc347561ff324f391daadcfea37ec6280
Jaydeep Gajera <jaydeepgajera63526@gmail.com>
2025-03-10 11:41:30 +0530

    Task B complete

---
+++ Ch-6/src/Tasks/TaskB/TaskB.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import {
+    BarLoader
+} from 'react-spinners';
+
+export default function TaskB() {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            // // "https://jsonplaceholder.typicode.com/users"
+            axios.get("https://jsonplaceholder.typicode.com/users")
+                .then((res) => {
+
+                    if (res.ok) {
+                        throw new Error("throw error : ")
+                    }
+
+                    setData(res.data)
+                    setLoading(false);
+                })
+                .catch((error) => {
+                    alert("API data is not fetched!!! " + error.message);
+                    setLoading(false);
+                });
+        }, 2000)
+    }, [])
+
+    if (loading) {
+        return (
+            <div>
+                <BarLoader loading={loading} style={{ textAlign: "center",display:"flex",flexDirection:"column",justifyContent:"center" }} />
+            </div>
+        )
+    }
+    return (
+        <>
+            <div>
+                {
+                    data.map((item) => {
+                        return (
+                            <div className='container card p-2 mt-3 '>
+
+                                <h2>User Name : {item.name}</h2>
+                                <p><b>User E-mail :</b> {item.email}</p>
+                                <p><b>Address :</b> {item.address.street} , {item.address.suite} , {item.address.city} , code: {item.address.zipcode}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
+}
