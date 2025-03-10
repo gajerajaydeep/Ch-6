b39e6b7d76094dc30114ac0a67bd19d183117752
Jaydeep Gajera <jaydeepgajera63526@gmail.com>
2025-03-10 18:39:47 +0530

    Task A-B complete

--- Ch-6/src/App.jsx
+++ Ch-6/src/App.jsx
@@ -1,12 +1,16 @@
 import React ,{ useState } from 'react'
 import TaskA from './Tasks/TaskA/TaskA'
+import TaskB from './Tasks/TaskB/TaskB'
+import TaskC from './Tasks/TaskC/TaskC'
 
 
 function App() {
 
   return (
     <>
-      <TaskA/>
+      {/* <TaskA/> */}
+      {/* <TaskB/> */}
+      <TaskC/>
     </>
   )
 }
